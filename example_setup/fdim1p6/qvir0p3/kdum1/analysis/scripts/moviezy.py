e2e36276bc6d8e2a1949839a3a02e1e70647c575
CFEsau <c.esau@shef.ac.uk>
2016-01-20 15:40:52 +0000

    added formatted time label to all 2D plots

--- example_setup/fdim1p6/qvir0p3/kdum1/analysis/scripts/moviezy.py
+++ example_setup/fdim1p6/qvir0p3/kdum1/analysis/scripts/moviezy.py
@@ -11,7 +11,8 @@ arglist = sys.argv
 sim = ''
 
 if(len(arglist) < 2):
-    sim  = str(raw_input('input the path to the simulation directory: ')) 
+    sim  = str(raw_input('input the path to the simulation directory: '))
+    #sim = 'analysis_test/outputs/runinv_0100/'
 else:
     sim = str(arglist[1])
 
@@ -51,24 +52,48 @@ for i in range(1,nfile+1):
     m=[]
     numbers=[]
     nstars=0
+#for finding the n most massive stars in the system:
+    most_massive=[]
+    n_list=20 #number of stars to go in list
+    time=0. #time of snapshot in Myr
+
     for line in lines:
         numbers.append(line.split())
         x.append(np.float64(numbers[nstars][4]))
         y.append(np.float64(numbers[nstars][5]))
         z.append(np.float64(numbers[nstars][6]))
         m.append(np.float64(numbers[nstars][3]))
+
         nstars+=1
+
+#mass shouldn't change between snapshots!
+#check list of most massive is the same in first and last:
+    if ifname==sim+'/snapshots'+'/snap0001':
+        most_massive=sorted(m)
+        most_massive=most_massive[-n_list:]
+        print 'After sorting snapshot 001: ', most_massive
+        #print 'final in list: ',most_massive[-1]
+    if ifname==sim+'/snapshots'+'/snap0999':
+        most_massive=sorted(m)
+        most_massive=most_massive[-n_list-1:]
+        print 'After sorting snapshot 999: ', most_massive
+
+    time=numbers[0][2]
+    #print time
+
     fo.close()
 
     plt.clf()
     plt.locator_params(nbins=16)
     ax1 = fig.add_subplot(111)
-    ax1.scatter(z, y,marker='.',s=2)
-    plt.xlabel('Z-Position (pc)')
+    ax1.scatter(x, y,marker='.',s=2)
+    plt.xlabel('X-Position (pc)')
     plt.ylabel('Y-Position (pc)')
     plt.title('Simulation')
     plt.grid(True)
     plt.axis([-5, 5, -5, 5])
+    textstring='Time: %.2f Myr' % (round(float(time),2))
+    plt.text(3, 4, textstring)
     plt.savefig(ifname+'.zy.png', dpi=my_dpi, bbox_inches='tight')
 
 os.system('avconv -y -r 15 -i ' + sim + '/snapshots/snap%04d.zy.png -s 1024x800 ' + sim + '/zy.mp4')
