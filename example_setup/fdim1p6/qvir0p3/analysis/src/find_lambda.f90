2631bf66301c6a28425008d42700e781e6939ff3
CFEsau <c.esau@shef.ac.uk>
2016-03-08 16:52:20 +0000

    nmst no longer decreases when star has escaped, and general readability improved

--- example_setup/fdim1p6/qvir0p3/analysis/src/find_lambda.f90
+++ example_setup/fdim1p6/qvir0p3/analysis/src/find_lambda.f90
@@ -10,35 +10,37 @@
 !This algorithm is presented in Allison et al, 2009, MNRAS, 395, 1449
 !
 
-subroutine find_lambda(snapshoti,ni)
-  use parameters_module
-  implicit none
-  integer, intent(in) :: snapshoti, ni !Snapshot number and star number
+SUBROUTINE find_lambda(snapshoti,ni)
+  USE parameters_module
+
+  IMPLICIT NONE
+
+  INTEGER, INTENT(in) :: snapshoti, ni !Snapshot number and star number
 ! mi,ri= mass, position & distance from com of stars
   DOUBLE PRECISION, DIMENSION(:), ALLOCATABLE :: mi, rcom_i
   DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: ri
-  real :: ti !time of snapshot
-  integer, dimension(:), allocatable :: mlist !ID numbers for heapsort
-  logical, dimension(:), allocatable :: i_incluster !has this star escaped?
+  REAL :: ti !time of snapshot
+  INTEGER, DIMENSION(:), ALLOCATABLE :: mlist !ID numbers for heapsort
+  LOGICAL, DIMENSION(:), ALLOCATABLE :: i_incluster !has this star escaped?
 
-  integer :: ndim !2D or 3D analysis
+  INTEGER :: ndim !2D or 3D analysis
   
-  double precision, dimension(:), allocatable :: length !length of each MST edge
-  double precision :: obj_mst !Length of object tree (e.g. most massive stars)
-  double precision, dimension(:,:), allocatable :: obj_r !positions of obj stars
-  double precision, DIMENSION(:), ALLOCATABLE :: x,y,z !split of obj_r
-  integer, dimension(:,:), allocatable :: node !node connections for mst
+  DOUBLE PRECISION, DIMENSION(:), ALLOCATABLE :: length !length of each MST edge
+  DOUBLE PRECISION :: obj_mst !Length of object tree (e.g. most massive stars)
+  DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: obj_r !positions of obj stars
+  DOUBLE PRECISION, DIMENSION(:), ALLOCATABLE :: x,y,z !split of obj_r
+  INTEGER, DIMENSION(:,:), ALLOCATABLE :: node !node connections for mst
 
-  integer :: nloop !number of random MSTs in loop
-  double precision, DIMENSION(:), ALLOCATABLE :: rand_mst !total lengths of random MSTs
+  INTEGER :: nloop !number of random MSTs in loop
+  DOUBLE PRECISION, DIMENSION(:), ALLOCATABLE :: rand_mst !total lengths of random MSTs
   INTEGER, DIMENSION(:), ALLOCATABLE :: rand_list !IDs of rand_mst list
-  real :: avranmst !Average length of random trees
-  real ::ranup, ranlow !Upper & lower boundaries, 1 sigma
+  REAL :: avranmst !Average length of random trees
+  REAL ::ranup, ranlow !Upper & lower boundaries, 1 sigma
 
   LOGICAL, DIMENSION(:), ALLOCATABLE :: done !record which stars have been randomly selected
   REAL :: rand
   
-  integer :: i,j,k !generic counters
+  INTEGER :: i,j,k !generic counters
 
 ! Allocate memory for arrays
   ALLOCATE(mi(1:ni))
@@ -46,89 +48,95 @@ subroutine find_lambda(snapshoti,ni)
   ALLOCATE(rcom_i(1:ni))
   ALLOCATE(mlist(1:ni))
   ALLOCATE(done(1:ni))
-  allocate(i_incluster(1:ni))
+!Allocate memory for arrays of length nmst:
+  ALLOCATE(length(1:nmst-1))
+  ALLOCATE(x(1:nmst))
+  ALLOCATE(y(1:nmst))
+  ALLOCATE(z(1:nmst))
+  ALLOCATE(node(1:nmst-1,1:2))
+  ALLOCATE(obj_r(1:nmst,1:3))
+
+
 ! And fill arrays 
   mi(1:ni)=m(snapshoti,1:ni)
   ri(1:ni,1:3)=r(snapshoti,1:ni,1:3)
   ti=t(snapshoti,1)
 
-!arrays are allocated later, in case some stars have escaped
-!except obj_r, which we need now. this is split up later.
-  allocate(obj_r(1:nmst,1:3))
-! first entry stores old IDs, second stores new
-  !allocate(obj_mass(1:2,1:nmst))
-  if (snapshoti==1) obj_mass(1,1:nmst)=0
+!====================================
+!For when outliers are being ignored
+!====================================
+!
+  ALLOCATE(i_incluster(1:ni))
+! first entry of obj_mass stores IDs of original most massive stars,
+! second stores new most-massive stars
+  IF (snapshoti==1) obj_mass(1,1:nmst)=0
+!=====================================
+
 
 !2D or 3D analysis?
   ndim = 2
-  if (ndim==2) then
+  IF (ndim==2) THEN
      rcom_i(1:ni)=r_com(snapshoti,1:ni,4)
      i_incluster(1:ni)=this_in2Dcluster(snapshoti,1:ni)
-  else if (ndim==3) then
+  ELSE IF (ndim==3) THEN
      rcom_i(1:ni)=r_com(snapshoti,1:ni,5)
      i_incluster(1:ni)=this_in3Dcluster(snapshoti,1:ni)
-  end if
+  END IF
 
-!======================================================================
-!Sort stars in order of mass & make selection
-!======================================================================
+!============================================================
+!Sort stars in order of mass & make selection of 'obj' stars
+!============================================================
 !
-
   obj_r=0.
   mlist=0
   
-  do j=1,ni
+!Assign IDs to stars
+  DO j=1,ni
      mlist(j)=j
-  end do
+  END DO
   
-  call heapsort(ni,mi,mlist)
+  CALL heapsort(ni,mi,mlist)
   
 !select nmst most massive stars:
 !(while checking whether star should be ignored)
   j=0 !j tracks with i but changes with each *attempted* iteration,
       ! even if i does not change
 
-  do i = 1,nmst
+  DO i = 1,nmst
 5    j=j+1
      k = mlist(ni+1-j) !heapsort orders from small to large - invert
      
 ! if star has escaped cluster, leave it out of the list
-     if (.not. i_incluster(k)) then
-        write(21,*) i, k, mi(k), rcom_i(k), i_incluster(k)
-        goto 5
-     end if
+     IF (.NOT. i_incluster(k)) THEN
+        WRITE(21,*) snapshoti, i, k, mi(k), rcom_i(k), i_incluster(k)
+        GOTO 5 !don't use 'cycle' as we don't want i to inrease
+     END IF
      obj_r(i,1) = ri(k,1) !x position
      obj_r(i,2) = ri(k,2) !y position
 !For 2D set z to 0
-     IF (ndim == 2) then
+     IF (ndim == 2) THEN
         obj_r(i,3) = 0. !z position
-     else if (ndim == 3) then
+     ELSE IF (ndim == 3) THEN
         obj_r(i,3) = ri(k,3) !z position 
-     else
-        stop 'ndim must be 2 or 3'
-     end if
+     ELSE
+        STOP 'ndim must be 2 or 3'
+     END IF
+
 ! track masses selected after each snapshot for output file:
      obj_mass(2,i) = mi(mlist(ni+1-j))
-  end do
+  END DO
 
 ! write snapshot, time, and list of object masses to output file
 ! when IDs of most massive stars change
-  do i=1, nmst
-     if(obj_mass(1,i) /= obj_mass(2,i)) then
-        write(20,99) snapshoti, ti, obj_mass(2,1:nmst)
-        exit
-     end if
-  end do
+  DO i=1, nmst
+     IF(obj_mass(1,i) /= obj_mass(2,i)) THEN
+        WRITE(20,99) snapshoti, ti, obj_mass(2,1:nmst)
+        EXIT
+     END IF
+  END DO
 99   FORMAT(1X,I4,2X,E9.3,*(2X,F8.3))
 
   obj_mass(1,1:nmst)=obj_mass(2,1:nmst)
-
-!Allocate memory for arrays of length nmst:
-  allocate(length(1:nmst-1))
-  allocate(x(1:nmst))
-  allocate(y(1:nmst))
-  allocate(z(1:nmst))
-  allocate(node(1:nmst-1,1:2))
 
 !======================================================================
 !Find the MST length for the OBJECT stars
@@ -141,7 +149,7 @@ subroutine find_lambda(snapshoti,ni)
    x(1:nmst)=obj_r(1:nmst,1)
    y(1:nmst)=obj_r(1:nmst,2)
    z(1:nmst)=obj_r(1:nmst,3)
-   !IF (z(1)==0) WRITE(6,*) '====***2D OBJECT MST***===='
+
    CALL mst(nmst,x,y,z,node,length)
    
    DO i = 1,nmst-1
@@ -155,6 +163,7 @@ subroutine find_lambda(snapshoti,ni)
 !For large nmst it can take a while to build the MST, so reduce
 !the number of times we do the loop - higher nmst MSTs are less 
 !stochstic anyway...
+
     nloop = 1000
     IF(nmst >= 100) nloop = 50
    
@@ -170,7 +179,7 @@ subroutine find_lambda(snapshoti,ni)
          k = NINT(rand*ni)
          IF (k == 0) GOTO 6    !There is no star with id=0
          IF (done(k)) GOTO 6    !Don't chose the same star twice
-         if (.not. i_incluster(k)) goto 6 !Don't use escaped stars
+         IF (.NOT. i_incluster(k)) GOTO 6 !Don't use escaped stars
          done(k) = .TRUE.       !You're selected
          x(i) = ri(k,1)
          y(i) = ri(k,2)
@@ -188,8 +197,6 @@ subroutine find_lambda(snapshoti,ni)
          rand_mst(j) = rand_mst(j) + length(i)    !Add the edges of the mst
       END DO                                  !to find the total length
    END DO
-
-   !IF (ndim == 2) WRITE(6,*) '====***2D RANDOM MST***===='
 
 !======================================================================
 !Finally - find the average value of the random MSTs and the significance
@@ -224,14 +231,14 @@ subroutine find_lambda(snapshoti,ni)
 !Deallocate arrays:
    DEALLOCATE(mi)
    DEALLOCATE(ri)
-   deallocate(i_incluster)
+   DEALLOCATE(i_incluster)
    DEALLOCATE(mlist)
    DEALLOCATE(done)
-   deallocate(length)
-   deallocate(obj_r)
-   deallocate(x)
-   deallocate(y)
-   deallocate(z)
-   deallocate(node)
+   DEALLOCATE(length)
+   DEALLOCATE(obj_r)
+   DEALLOCATE(x)
+   DEALLOCATE(y)
+   DEALLOCATE(z)
+   DEALLOCATE(node)
 
  END SUBROUTINE find_lambda
