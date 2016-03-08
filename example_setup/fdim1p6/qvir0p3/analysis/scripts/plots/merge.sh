e374214cc18eaf64cb02fea44f461024b17644a2
CFEsau <c.esau@shef.ac.uk>
2016-03-08 17:41:31 +0000

    plots for other lambda calculations added

--- example_setup/fdim1p6/qvir0p3/analysis/scripts/plots/merge.sh
+++ example_setup/fdim1p6/qvir0p3/analysis/scripts/plots/merge.sh
@@ -3,7 +3,7 @@
 echo ""
 echo "Combining plots..."
 echo ""
-sleep 0.8
+sleep 0.6
 outpath='../outputs'
 
 
@@ -19,7 +19,7 @@ done
 echo "Saved in all_energies.pdf"
 pdfunite ${flist} ${outpath}/all_energies.pdf
 echo ""
-sleep 0.8
+sleep 0.6
 
 
 flist=''
@@ -34,7 +34,7 @@ done
 echo "Saved in all_lagrangian.pdf"
 pdfunite ${flist} ${outpath}/all_lagrangian.pdf
 echo ""
-sleep 0.8
+sleep 0.6
 
 
 flist=''
@@ -50,7 +50,7 @@ done
 echo "Saved in all_virial.pdf"
 pdfunite ${flist} ${outpath}/all_virial.pdf
 echo ""
-sleep 0.8
+sleep 0.6
 
 
 flist=''
@@ -65,6 +65,66 @@ done
 echo "Saved in all_lambda.pdf"
 pdfunite ${flist} ${outpath}/all_lambda.pdf
 echo ""
-sleep 0.8
+sleep 0.6
+
+
+flist=''
+tot=0
+for file in ${outpath}'/'k*lambdabar.pdf; do
+    echo ${file}
+    flist="${flist} ${file}"
+    let 'tot+=1'
+    #echo "flist is ${flist}"
+done
+
+echo "Saved in all_lambdabar.pdf"
+pdfunite ${flist} ${outpath}/all_lambdabar.pdf
+echo ""
+sleep 0.6
+
+
+flist=''
+tot=0
+for file in ${outpath}'/'k*lambdatilde.pdf; do
+    echo ${file}
+    flist="${flist} ${file}"
+    let 'tot+=1'
+    #echo "flist is ${flist}"
+done
+
+echo "Saved in all_lambdatilde.pdf"
+pdfunite ${flist} ${outpath}/all_lambdatilde.pdf
+echo ""
+sleep 0.6
+
+
+flist=''
+tot=0
+for file in ${outpath}'/'k*lambdastar.pdf; do
+    echo ${file}
+    flist="${flist} ${file}"
+    let 'tot+=1'
+    #echo "flist is ${flist}"
+done
+
+echo "Saved in all_lambdastar.pdf"
+pdfunite ${flist} ${outpath}/all_lambdastar.pdf
+echo ""
+sleep 0.6
+
+
+flist=''
+tot=0
+for file in ${outpath}'/'k*gamma.pdf; do
+    echo ${file}
+    flist="${flist} ${file}"
+    let 'tot+=1'
+    #echo "flist is ${flist}"
+done
+
+echo "Saved in all_gamma.pdf"
+pdfunite ${flist} ${outpath}/all_gamma.pdf
+echo ""
+sleep 0.6
 
 echo "       ... done."
