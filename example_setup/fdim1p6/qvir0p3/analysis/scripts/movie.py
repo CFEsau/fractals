def3c64de2c49124fdd1bbe266cbfe88d5a72ef8
CFEsau <c.esau@shef.ac.uk>
2016-06-15 14:46:24 +0100

    added additional two 2D projections to movies script

--- example_setup/fdim1p6/qvir0p3/analysis/scripts/movie.py
+++ example_setup/fdim1p6/qvir0p3/analysis/scripts/movie.py
@@ -11,15 +11,14 @@ import time
 
 picture_count = int(0)
 ncores = int(0)
-projection=str('2D')
+#projection=str('xy')
+projection = raw_input("Enter projection: ")
 
-def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval):
+def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box):
         import matplotlib.pyplot as plt
 	from mpl_toolkits.mplot3d import Axes3D
 	import numpy as np
 
-	xy_box = int(5)
-	z_box = (1)
 	my_dpi=96
 
 	fig = plt.figure(figsize=(960/my_dpi, 960/my_dpi), dpi=my_dpi)
@@ -36,11 +35,13 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval):
 	vz=[]
 	m=[]
 	numbers=[]
-	cluster_2D =[]
-	cluster_3D =[]
 	nstars=0
 	n_list=int(10)
 	# This defines the nth largest sample
+	x_cluster=[]
+	y_cluster=[]
+	z_cluster=[]
+	m_cluster=[]
 	time=0. #time of snapshot in Myr
 	for line in lines:
 		numbers.append(line.split()) #This is tuple of whole table
@@ -48,21 +49,14 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval):
 		y = np.append(y,float(numbers[nstars][5]))
 		z = np.append(z,float(numbers[nstars][6]))
 		m = np.append(m,float(numbers[nstars][3]))
-		cluster_2D = np.append(cluster_2D, str(numbers[nstars][10]))		
-		cluster_3D = np.append(cluster_3D, str(numbers[nstars][11]))				
+        
+		x_cluster = np.append(x_cluster,float(numbers[nstars][4]))
+		y_cluster = np.append(y_cluster,float(numbers[nstars][5]))
+		z_cluster = np.append(z_cluster,float(numbers[nstars][6]))
+		m_cluster = np.append(m_cluster,float(numbers[nstars][3]))
+
 		nstars+=1
-	
-	if projection == "3D":		
-		x_cluster = x[np.where(cluster_3D=="T")]
-		y_cluster = y[np.where(cluster_3D=="T")]
-		z_cluster = z[np.where(cluster_3D=="T")]		
-		m_cluster = m[np.where(cluster_3D=="T")]
-	
-	elif projection == "2D":		
-		x_cluster = x[np.where(cluster_2D=="T")]
-		y_cluster = y[np.where(cluster_2D=="T")]
-		m_cluster = m[np.where(cluster_2D=="T")]		
-	
+
 	time=numbers[0][2] #time of snapshot is 1st row (any will do), 2nd col
 	#print time
 	mass_srt_by_m = sorted(m_cluster)
@@ -79,25 +73,47 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval):
 
 	if projection == "3D":
 		ax1 = fig.add_subplot(111, projection='3d')
-		ax1.set_zlabel('Z-Position (pc)')
+		ax1.set_xlabel('X-Position (pc)')
 		ax1.set_ylabel('Y-Position (pc)')
-		ax1.set_zlim(-1*xy_box,xy_box)		
+		ax1.set_zlabel('Z-Position (pc)')
+		ax1.set_xlim(-1*xy_box,xy_box)
 		ax1.set_ylim(-1*z_box,z_box)
+		ax1.set_zlim(-1*xy_box,xy_box)
 		ax1.scatter(x,z, y, marker='.',color='black', s=2, alpha=1.)
-		ax1.scatter(x_cluster[np.where(m_cluster>=mass_selected[0])],z_cluster[np.where(m_cluster>=mass_selected[0])], y_cluster[np.where(m_cluster>=mass_selected[0])], marker='*',color='red', s=20)	
+		ax1.scatter(x_cluster[np.where(m_cluster>=mass_selected[0])],z_cluster[np.where(m_cluster>=mass_selected[0])], y_cluster[np.where(m_cluster>=mass_selected[0])], marker='*',color='red', s=20)
 					
-	elif projection=="2D":
+	elif projection=="xy":
 	
 		ax1 = fig.add_subplot(111)
-		ax1.set_ylabel('Y-Position (pc)')		
+		ax1.set_xlabel('X-Position (pc)')
+		ax1.set_ylabel('Y-Position (pc)')
+		ax1.set_xlim(-1*xy_box,xy_box)
 		ax1.set_ylim(-1*xy_box,xy_box)
 		ax1.scatter(x, y, marker='.',color='black', s=20, alpha=0.3)
 		
-		ax1.scatter(x_cluster[np.where(m_cluster>=mass_selected[0])], y_cluster[np.where(m_cluster>=mass_selected[0])], marker='*',color='red', s=20)	
-	
+		ax1.scatter(x_cluster[np.where(m_cluster>=mass_selected[0])], y_cluster[np.where(m_cluster>=mass_selected[0])], marker='*',color='red', s=20)
 
-	ax1.set_xlabel('X-Position (pc)')
-	ax1.set_xlim(-1*xy_box,xy_box)
+	elif projection=="xz":
+	
+		ax1 = fig.add_subplot(111)
+		ax1.set_xlabel('X-Position (pc)')
+		ax1.set_ylabel('Z-Position (pc)')
+		ax1.set_xlim(-1*xy_box,xy_box)		
+		ax1.set_ylim(-1*xy_box,xy_box)
+		ax1.scatter(x, z, marker='.',color='black', s=20, alpha=0.3)
+		
+		ax1.scatter(x_cluster[np.where(m_cluster>=mass_selected[0])], z_cluster[np.where(m_cluster>=mass_selected[0])], marker='*',color='red', s=20)
+
+	elif projection=="yz":
+	
+		ax1 = fig.add_subplot(111)
+		ax1.set_xlabel('Y-Position (pc)')
+		ax1.set_ylabel('Z-Position (pc)')
+		ax1.set_xlim(-1*xy_box,xy_box)		
+		ax1.set_ylim(-1*xy_box,xy_box)
+		ax1.scatter(y, z, marker='.',color='black', s=20, alpha=0.3)
+		
+		ax1.scatter(y_cluster[np.where(m_cluster>=mass_selected[0])], z_cluster[np.where(m_cluster>=mass_selected[0])], marker='*',color='red', s=20)
 
 
 
@@ -110,14 +126,12 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval):
         plt.text(0.94*xy_box,-1.1*xy_box,kval,fontsize=10)
 
 
-	if projection =="3D":
-		plt.savefig(str(ifname)+'.xyz.png', dpi=my_dpi, bbox_inches='tight')
-	elif projection == "2D":
-		plt.savefig(str(ifname)+'.xy.png', dpi=my_dpi, bbox_inches='tight')
+        plt.savefig(str(ifname)+'.'+projection+'_'+str(xy_box)+'pc.png', dpi=my_dpi, bbox_inches='tight')
         plt.close()
 	del(mass_srt_by_m)
 	del(mass_selected)
         return snapname
+
 
 arglist = sys.argv
 sim = ''
@@ -130,13 +144,16 @@ else:
 	sim = str(arglist[1])
 
 kval = sim.split("_")[1] #get k01, k02, etc
+#Size of plot in pc:
+xy_box = int(5)
+z_box = (1)
 
 picture_count = int(0)
 nfile=0
 fdim = raw_input("fdim: ")
 qvir = raw_input("qvir: ")
 
-ncores = int(raw_input("Enter number of cores (Enter -1 to turn off parallisation, 0 to autodetect system cores): "))
+ncores = int(raw_input("Enter number of cores (Enter -1 to turn off parallelisation, 0 to autodetect system cores): "))
 
 if (ncores>0):
 
@@ -187,19 +204,20 @@ for i in range(1,nfile+1):
 	del zeros
 	
 	if ncores==-1:
-   		generate_snapshot(ifname[i-1],snapname[i-1],projection,qvir,fdim,kval)
+   		generate_snapshot(ifname[i-1],snapname[i-1],projection,qvir,fdim,kval,xy_box,z_box)
 
 if ncores!=-1:   	
 	irange = np.arange(0, i-1, 1)
-	jobs = [(ix, job_server.submit(generate_snapshot,args=(ifname[ix],snapname[ix],projection,qvir,fdim,kval),depfuncs=(), modules=("matplotlib","numpy","mpl_toolkits"), globals=globals())) for ix in irange]
+	jobs = [(ix, job_server.submit(generate_snapshot,args=(ifname[ix],snapname[ix],projection,qvir,fdim,kval,xy_box,z_box),depfuncs=(), modules=("matplotlib","numpy","mpl_toolkits"), globals=globals())) for ix in irange]
 	for ix, job in jobs:
            px=job()
            del(px)
 	   print snapname[ix]
 	   #print "Snapshot", ifname[ix], "is", job()
 
-os.system('avconv -y -r 24 -i ' + sim + '/snapshots/snap%04d.xy.png -s 1024x800 ' + sim + '/xy_' + kval + '.mp4')
+os.system('avconv -y -r 24 -i ' + sim + '/snapshots/snap%04d.' + projection + '_' + str(xy_box) + 'pc.png -s 1024x800 ' + sim + '/' + projection + '_' + str(kval) + '_' + str(xy_box) + 'pc.mp4')
 
-for fn in os.listdir(sim + '/snapshots/'):
-    if '.xy.png' in fn or '.xyz.png' in fn:
-        os.system("rm -rf " + sim + '/snapshots/' + fn)
+#Remove png files:
+#for fn in os.listdir(sim + '/snapshots/'):
+    #if 'pc.png' in fn:
+        #os.system("rm -rf " + sim + '/snapshots/' + fn)
