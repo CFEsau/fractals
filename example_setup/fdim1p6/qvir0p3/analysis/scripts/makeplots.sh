d7656b13acba1a33a9bd99944eeb5182e839d064
CFEsau <c.esau@shef.ac.uk>
2016-08-23 17:15:28 +0100

    added option in makeplots to skip non-lambda plots

--- example_setup/fdim1p6/qvir0p3/analysis/scripts/makeplots.sh
+++ example_setup/fdim1p6/qvir0p3/analysis/scripts/makeplots.sh
@@ -17,50 +17,89 @@ echo "**************************"
 echo "*      Making plots      *"
 echo "**************************"
 
+#Plotting energies etc takes a while. Most of the time
+#we're only interested in lambda. Option to skip other
+#types of plots and just do lambda.
+
+defaultskip="y"
+
+echo -n "Make only lambda plots? [y/n]"
+if ! read -t 6 skip; then
+    echo "No response. Default is:"
+    if [ "$defaultskip" = "y" ]; then
+	skip="y"
+	echo "        make only lambda plots"
+    else
+	skip="n"
+	echo "        make all plots"
+    fi
+    sleep 0.8
+fi
+
 echo ""
 echo "------------"
 echo "-- Energy --"
 echo "------------"
-python plots/energyplots.py ${fbin} ${fdim} ${qvir}
 
-param=E
-cluster='all'
-. ./plots/merge.sh
+if [ "$skip" = "n" ]; then
 
+    python plots/energyplots.py ${fbin} ${fdim} ${qvir}
+
+    param=E
+    cluster='all'
+    . ./plots/merge.sh
+
+else
+    echo "   Skipping plots"
+fi
 
 echo ""
 echo "------------------"
 echo "-- Virial Ratio --"
 echo "------------------"
-python plots/virialplots.py ${fbin} ${fdim} ${qvir}
 
-param=Qvir
-cluster='all'
-. ./plots/merge.sh
+if [ "$skip" = "n" ]; then
+
+    python plots/virialplots.py ${fbin} ${fdim} ${qvir}
+
+    param=Qvir
+    cluster='all'
+    . ./plots/merge.sh
+
+else
+    echo "   Skipping plots"
+fi
 
 
 echo ""
 echo "----------------------"
 echo "-- Half-mass radius --"
 echo "----------------------"
-python plots/lagrangeplots.py ${fbin} ${fdim} ${qvir}
 
-param=Rh
-cluster='all'
-. ./plots/merge.sh
-cluster='FoV5pc'
-. ./plots/merge.sh
-cluster='r2rhalf'
-. ./plots/merge.sh
-cluster='r3rhalf'
-. ./plots/merge.sh
+if [ "$skip" = "n" ]; then
+    python plots/lagrangeplots.py ${fbin} ${fdim} ${qvir}
 
+    param=Rh
+    cluster='all'
+    . ./plots/merge.sh
+    cluster='FoV5pc'
+    . ./plots/merge.sh
+    cluster='r2rhalf'
+    . ./plots/merge.sh
+    cluster='r3rhalf'
+    . ./plots/merge.sh
+
+else
+    echo "   Skipping plots"
+fi
 
 echo ""
 echo "------------"
 echo "-- Lambda --"
 echo "------------"
-python plots/lambdaplots.py ${fbin} ${fdim} ${qvir}
+# (Plotting comparisons between different projections)
+
+python plots/lambdaproj.py ${fbin} ${fdim} ${qvir}
 
 param=lambar
 cluster='all'
@@ -111,6 +150,8 @@ echo ""
 echo "****************************"
 echo "* Comparing lambda methods *"
 echo "****************************"
+# (Plotting lambar, lamtilde, etc against each other)
+
 plots/differentlambda.py ${fbin} ${fdim} ${qvir}
 
 param=lam
@@ -131,17 +172,21 @@ echo ""
 echo "************************************"
 echo "* Making comparison plots across k *"
 echo "************************************"
-plots/energy_comparek.py ${fbin} ${fdim} ${qvir}
-echo ""
-sleep 0.6
+# (Plotting all values of k together for each E, Q, lambda, etc)
 
-plots/virial_comparek.py ${fbin} ${fdim} ${qvir}
-echo ""
-sleep 0.6
+if [ "$skip" = "n" ]; then
+    plots/energy_comparek.py ${fbin} ${fdim} ${qvir}
+    echo ""
+    sleep 0.6
 
-plots/lagrange_comparek.py ${fbin} ${fdim} ${qvir}
-echo ""
-sleep 0.6
+    plots/virial_comparek.py ${fbin} ${fdim} ${qvir}
+    echo ""
+    sleep 0.6
+
+    plots/lagrange_comparek.py ${fbin} ${fdim} ${qvir}
+    echo ""
+    sleep 0.6
+fi
 
 plots/lambda_comparek.py ${fbin} ${fdim} ${qvir}
 echo ""
