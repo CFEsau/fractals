7a0f59a3e8defaff63b505838380124197eecdfd
CFEsau <c.esau@shef.ac.uk>
2016-11-02 16:33:00 +0000

    added new movie script that centres on the centre of mass rather than (0,0,0) but c of m file must be in csv format. I've not done this yet in the main code so keeping original movie script in as movie_old.

--- example_setup/fdim1p6/qvir0p3/analysis/scripts/movie_old.py
+++ example_setup/fdim1p6/qvir0p3/analysis/scripts/movie_old.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-#This script traces the centre of mass of the grid
-#(Note c of m files must be in csv format)
 
 import os
 import matplotlib.pyplot as plt
@@ -16,7 +14,7 @@ ncores = int(0)
 #projection=str('xy')
 projection = raw_input("Enter projection: ")
 
-def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fcom_elements):
+def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box):
         import matplotlib.pyplot as plt
 	from mpl_toolkits.mplot3d import Axes3D
 	import numpy as np
@@ -47,22 +45,15 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fco
 	time=0. #time of snapshot in Myr
 	for line in lines:
 		numbers.append(line.split()) #This is tuple of whole table
-		
-		if (abs(float(numbers[nstars][4]))<xy_box) and (abs(float(numbers[nstars][5]))<xy_box):
-		
-			x = np.append(x,float(numbers[nstars][4]))
-			y = np.append(y,float(numbers[nstars][5]))
-			x_cluster = np.append(x_cluster,float(numbers[nstars][4]))
-			y_cluster = np.append(y_cluster,float(numbers[nstars][5]))
-			
-			if (projection!="xy"):
-				z = np.append(z,float(numbers[nstars][6]))
-				m = np.append(m,float(numbers[nstars][3]))
-				z_cluster = np.append(z_cluster,float(numbers[nstars][6]))
-				m_cluster = np.append(m_cluster,float(numbers[nstars][3]))
-			else:
-				m = np.append(m,float(numbers[nstars][3]))
-				m_cluster = np.append(m_cluster,float(numbers[nstars][3]))		        
+		x = np.append(x,float(numbers[nstars][4]))
+		y = np.append(y,float(numbers[nstars][5]))
+		z = np.append(z,float(numbers[nstars][6]))
+		m = np.append(m,float(numbers[nstars][3]))
+        
+		x_cluster = np.append(x_cluster,float(numbers[nstars][4]))
+		y_cluster = np.append(y_cluster,float(numbers[nstars][5]))
+		z_cluster = np.append(z_cluster,float(numbers[nstars][6]))
+		m_cluster = np.append(m_cluster,float(numbers[nstars][3]))
 
 		nstars+=1
 
@@ -78,11 +69,8 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fco
 	#print "SORTED", np.where(m_cluster>=mass_selected[0])
 
 	fo.close()
-	
-	fcom_element_x=float(fcom_elements[1])
-	fcom_element_y=float(fcom_elements[2])
-	
-	print (fcom_element_x, fcom_element_y)
+
+
 	plt.clf()
 
 	plt.locator_params(nbins=16)
@@ -101,21 +89,14 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fco
 	elif projection=="xy":
 	
 		ax1 = fig.add_subplot(111)
-		ax1.set_xlabel('X-Position w.r.t COM (pc)')
-		ax1.set_ylabel('Y-Position w.r.t COM (pc)')
+		ax1.set_xlabel('X-Position (pc)')
+		ax1.set_ylabel('Y-Position (pc)')
 		ax1.set_xlim(-1*xy_box,xy_box)
 		ax1.set_ylim(-1*xy_box,xy_box)
+		ax1.scatter(x, y, marker='.',color='black', s=20, alpha=0.3)
 		
-		#The next two lines overplot an axis describing the geometric centre of the cluster
-		ax1.axhline((-1*fcom_element_y), linewidth=2,alpha=0.15, color='g')
-		ax1.axvline((-1*fcom_element_x), linewidth=2,alpha=0.15, color='g')
+		ax1.scatter(x_cluster[np.where(m_cluster>=mass_selected[0])], y_cluster[np.where(m_cluster>=mass_selected[0])], marker='*',color='red', s=20)
 
-		# This is where the data are plotted		
-		ax1.scatter((x-fcom_element_x), (y-fcom_element_y), marker='.',color='black', s=20, alpha=0.3)		
-		ax1.scatter((x_cluster[np.where(m_cluster>=mass_selected[0])]-fcom_element_x), (y_cluster[np.where(m_cluster>=mass_selected[0])]-fcom_element_y), marker='*',color='red', s=20)
-
-
-		
 	elif projection=="xz":
 	
 		ax1 = fig.add_subplot(111)
@@ -143,9 +124,8 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fco
 	textstring='Time: %.2f Myr' % (round(float(time),2))
 	plt.title(textstring)
 	plt.grid(True)
-        plt.text(0.70*xy_box,0.95*xy_box,"qvir = " + qvir)
-        plt.text(0.70*xy_box,0.9*xy_box,"fdim = " + fdim)
-        plt.text(0.70*xy_box,0.85*xy_box,"N_Stars = " + str(len(x_cluster[np.where(m_cluster>=mass_selected[0])])))
+        plt.text(0.75*xy_box,0.95*xy_box,"qvir = " + qvir)
+        plt.text(0.75*xy_box,0.9*xy_box,"fdim = " + fdim)
         plt.text(-1.1*xy_box,-1.1*xy_box,"fbin = 0%",fontsize=11)
         plt.text(0.94*xy_box,-1.1*xy_box,kval,fontsize=10)
 
@@ -217,8 +197,6 @@ elif (ncores==0):
 		print '---------------------------------------------'
 		
 		time.sleep(2)
-
-
 		
 for fname in os.listdir(sim + '/snapshots/'):
     if 'snap' in fname and not 'tmp' in fname and not 'png' in fname:
@@ -228,26 +206,6 @@ picture_count = int(0)
 ifname=[]
 snapname=[]
 
-# The lines here read in the centre of mass data from the COM data file
-# it reads in each line of data into an single element in the list fcom_data
-# this list will be separated by the delimiter later on, for now, it's easier
-# to keep it as a single set of contiguous data.
-
-fcom_file = open('c_of_m_xy.txt', 'r')
-fcom_data=[]
-for line in fcom_file:
-	fcom_data.append(line.strip('\n'))
-	
-	del(line)
-	
-
-fcom_elements=[]
-
-for fcom_datum in fcom_data:
-
-	fcom_elements.append(fcom_datum.replace("'",'').split(',')[1:])
-
-
 for i in range(1,nfile+1):
 	zeros=''
 	for kx in range (0,(3 - int(np.floor(np.log10(i))))):
@@ -255,14 +213,13 @@ for i in range(1,nfile+1):
         ifname.append(str(sim + '/snapshots/' + '/snap'+ str(zeros) + str(i)))
 	snapname.append('snap'+ str(zeros) + str(i))
 	del zeros
-
-#	print fcom_elements
+	
 	if ncores==-1:
-   		generate_snapshot(ifname[i-1],snapname[i-1],projection,qvir,fdim,kval,xy_box,z_box,fcom_elements[i])
+   		generate_snapshot(ifname[i-1],snapname[i-1],projection,qvir,fdim,kval,xy_box,z_box)
 
 if ncores!=-1:   	
 	irange = np.arange(0, i-1, 1)
-	jobs = [(ix, job_server.submit(generate_snapshot,args=(ifname[ix],snapname[ix],projection,qvir,fdim,kval,xy_box,z_box,fcom_elements[ix]),depfuncs=(), modules=("matplotlib","numpy","mpl_toolkits"), globals=globals())) for ix in irange]
+	jobs = [(ix, job_server.submit(generate_snapshot,args=(ifname[ix],snapname[ix],projection,qvir,fdim,kval,xy_box,z_box),depfuncs=(), modules=("matplotlib","numpy","mpl_toolkits"), globals=globals())) for ix in irange]
 	for ix, job in jobs:
            px=job()
            del(px)
