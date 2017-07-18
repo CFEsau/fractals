5ec7445821f39a184075334c500ea7d24cdd1aab
CFEsau <c.esau@shef.ac.uk>
2017-07-18 14:08:12 +0100

    changed script to read in data from output without need for csv format

--- example_setup/graphics/animations/movie.py
+++ example_setup/graphics/animations/movie.py
@@ -4,21 +4,22 @@
 
 import os
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import sys
 import pp
 import time
+#from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.axes3d
 #Global Variables#####
 
 picture_count = int(0)
 ncores = int(0)
-#projection=str('xy')
-projection = raw_input("Enter projection: ")
+#proj=str('xy')
+proj = raw_input("Enter projection: ")
 
-def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fcom_elements):
+def generate_snapshot(ifname,snapname,proj,qvir,fdim,kval,xy_box,z_box,fcom_elements):
         import matplotlib.pyplot as plt
-	from mpl_toolkits.mplot3d import Axes3D
+#	from mpl_toolkits.mplot3d import Axes3D
 	import numpy as np
 
 	my_dpi=96
@@ -55,7 +56,7 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fco
 			x_cluster = np.append(x_cluster,float(numbers[nstars][4]))
 			y_cluster = np.append(y_cluster,float(numbers[nstars][5]))
 			
-			if (projection!="xy"):
+			if (proj!="xy"):
 				z = np.append(z,float(numbers[nstars][6]))
 				m = np.append(m,float(numbers[nstars][3]))
 				z_cluster = np.append(z_cluster,float(numbers[nstars][6]))
@@ -82,12 +83,12 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fco
 	fcom_element_x=float(fcom_elements[1])
 	fcom_element_y=float(fcom_elements[2])
 	
-	print (fcom_element_x, fcom_element_y)
+	#print (fcom_element_x, fcom_element_y)
 	plt.clf()
 
 	plt.locator_params(nbins=16)
 
-	if projection == "3D":
+	if proj == "3D":
 		ax1 = fig.add_subplot(111, projection='3d')
 		ax1.set_xlabel('X-Position (pc)')
 		ax1.set_ylabel('Y-Position (pc)')
@@ -96,9 +97,11 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fco
 		ax1.set_ylim(-1*z_box,z_box)
 		ax1.set_zlim(-1*xy_box,xy_box)
 		ax1.scatter(x,z, y, marker='.',color='black', s=2, alpha=1.)
-		ax1.scatter(x_cluster[np.where(m_cluster>=mass_selected[0])],z_cluster[np.where(m_cluster>=mass_selected[0])], y_cluster[np.where(m_cluster>=mass_selected[0])], marker='*',color='red', s=20)
+		ax1.scatter(x_cluster[np.where(m_cluster>=mass_selected[0])],
+                            z_cluster[np.where(m_cluster>=mass_selected[0])],
+                            y_cluster[np.where(m_cluster>=mass_selected[0])], marker='*',color='red', s=20)
 					
-	elif projection=="xy":
+	elif proj=="xy":
 	
 		ax1 = fig.add_subplot(111)
 		ax1.set_xlabel('X-Position w.r.t COM (pc)')
@@ -112,11 +115,13 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fco
 
 		# This is where the data are plotted		
 		ax1.scatter((x-fcom_element_x), (y-fcom_element_y), marker='.',color='black', s=20, alpha=0.3)		
-		ax1.scatter((x_cluster[np.where(m_cluster>=mass_selected[0])]-fcom_element_x), (y_cluster[np.where(m_cluster>=mass_selected[0])]-fcom_element_y), marker='*',color='red', s=20)
+		ax1.scatter((x_cluster[np.where(m_cluster>=mass_selected[0])]-fcom_element_x),
+                            (y_cluster[np.where(m_cluster>=mass_selected[0])]-fcom_element_y),
+                            marker='*',color='red', s=20)
 
 
 		
-	elif projection=="xz":
+	elif proj=="xz":
 	
 		ax1 = fig.add_subplot(111)
 		ax1.set_xlabel('X-Position (pc)')
@@ -125,9 +130,11 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fco
 		ax1.set_ylim(-1*xy_box,xy_box)
 		ax1.scatter(x, z, marker='.',color='black', s=20, alpha=0.3)
 		
-		ax1.scatter(x_cluster[np.where(m_cluster>=mass_selected[0])], z_cluster[np.where(m_cluster>=mass_selected[0])], marker='*',color='red', s=20)
+		ax1.scatter(x_cluster[np.where(m_cluster>=mass_selected[0])],
+                            z_cluster[np.where(m_cluster>=mass_selected[0])],
+                            marker='*',color='red', s=20)
 
-	elif projection=="yz":
+	elif proj=="yz":
 	
 		ax1 = fig.add_subplot(111)
 		ax1.set_xlabel('Y-Position (pc)')
@@ -136,21 +143,24 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fco
 		ax1.set_ylim(-1*xy_box,xy_box)
 		ax1.scatter(y, z, marker='.',color='black', s=20, alpha=0.3)
 		
-		ax1.scatter(y_cluster[np.where(m_cluster>=mass_selected[0])], z_cluster[np.where(m_cluster>=mass_selected[0])], marker='*',color='red', s=20)
+		ax1.scatter(y_cluster[np.where(m_cluster>=mass_selected[0])],
+                            z_cluster[np.where(m_cluster>=mass_selected[0])],
+                            marker='*',color='red', s=20)
 
 
-
-	textstring='Time: %.2f Myr' % (round(float(time),2))
-	plt.title(textstring)
-	plt.grid(True)
-        plt.text(0.70*xy_box,0.95*xy_box,"qvir = " + qvir)
-        plt.text(0.70*xy_box,0.9*xy_box,"fdim = " + fdim)
-        plt.text(0.70*xy_box,0.85*xy_box,"N_Stars = " + str(len(x_cluster[np.where(m_cluster>=mass_selected[0])])))
-        plt.text(-1.1*xy_box,-1.1*xy_box,"fbin = 0%",fontsize=11)
-        plt.text(0.94*xy_box,-1.1*xy_box,kval,fontsize=10)
-
-
-        plt.savefig(str(ifname)+'.'+projection+'_'+str(xy_box)+'pc.png', dpi=my_dpi, bbox_inches='tight')
+        if proj != '3D':
+	        textstring='Time: %.2f Myr' % (round(float(time),2))
+	        plt.title(textstring)
+	        plt.grid(True)
+                plt.text(0.70*xy_box,0.95*xy_box,"qvir = " + qvir)
+                plt.text(0.70*xy_box,0.9*xy_box,"fdim = " + fdim)
+                plt.text(0.70*xy_box,0.85*xy_box,"N_Stars = " +
+                         str(len(x_cluster[np.where(m_cluster>=mass_selected[0])])))
+                plt.text(-1.1*xy_box,-1.1*xy_box,"fbin = 0%",fontsize=11)
+                plt.text(0.94*xy_box,-1.1*xy_box,kval,fontsize=10)
+        
+        
+        plt.savefig(str(ifname)+'.'+proj+'_'+str(xy_box)+'pc.png', dpi=my_dpi, bbox_inches='tight')
         plt.close()
 	del(mass_srt_by_m)
 	del(mass_selected)
@@ -164,25 +174,30 @@ def generate_snapshot(ifname,snapname,projection,qvir,fdim,kval,xy_box,z_box,fco
 
 arglist = sys.argv
 sim = ''
-if(len(arglist) < 2):
+##if(len(arglist) < 2):
+##
+##	sim = raw_input("Enter file path: ")
+##	#sim = 'analysis_test/outputs/runinv_k01'
+##else:
+##
+##	sim = str(arglist[1])
 
-	sim = raw_input("Enter file path: ")
-	#sim = 'analysis_test/outputs/runinv_k01'
-else:
 
-	sim = str(arglist[1])
+kval = raw_input("Enter kval: ")
+
+sim = '../../fbin0p0/fdim1p6/qvir0p3/outputs/runinv_k'+str(kval)
 
 #print 'sim:',sim
-
-kval = sim.split("_")[1] #get k01, k02, etc
-#Size of plot in pc:
-xy_box = int(5)
-z_box = (1)
 
 picture_count = int(0)
 nfile=0
 fdim = raw_input("fdim: ")
 qvir = raw_input("qvir: ")
+
+##kval = sim.split("_")[1] #get k01, k02, etc
+#Size of plot in pc:
+xy_box = int(5)
+z_box = (1)
 
 ncores = int(raw_input("Enter number of cores (Enter -1 to turn off parallelisation, 0 to autodetect system cores): "))
 
@@ -233,7 +248,7 @@ snapname=[]
 # this list will be separated by the delimiter later on, for now, it's easier
 # to keep it as a single set of contiguous data.
 
-fcom_file = open('c_of_m_xy.txt', 'r')
+fcom_file = open(sim+'/cluster_FoV5pc/c_of_m_xy.dat', 'r')
 fcom_data=[]
 for line in fcom_file:
 	fcom_data.append(line.strip('\n'))
@@ -244,8 +259,9 @@ for line in fcom_file:
 fcom_elements=[]
 
 for fcom_datum in fcom_data:
-
-	fcom_elements.append(fcom_datum.replace("'",'').split(',')[1:])
+	fcom_elements.append(fcom_datum.replace(" ",",").replace(',,',',').replace(',,',',').split(',')[1:])
+	#print(fcom_datum.replace(" ",",").replace(',,',',').replace(',,',',').split(',')[1:])
+	#fcom_elements.append(fcom_datum.replace("'",'').split(',')[1:])
 
 
 for i in range(1,nfile+1):
@@ -258,11 +274,13 @@ for i in range(1,nfile+1):
 
 #	print fcom_elements
 	if ncores==-1:
-   		generate_snapshot(ifname[i-1],snapname[i-1],projection,qvir,fdim,kval,xy_box,z_box,fcom_elements[i])
+   		generate_snapshot(ifname[i-1],snapname[i-1],proj,qvir,fdim,kval,xy_box,z_box,fcom_elements[i])
 
 if ncores!=-1:   	
 	irange = np.arange(0, i-1, 1)
-	jobs = [(ix, job_server.submit(generate_snapshot,args=(ifname[ix],snapname[ix],projection,qvir,fdim,kval,xy_box,z_box,fcom_elements[ix]),depfuncs=(), modules=("matplotlib","numpy","mpl_toolkits"), globals=globals())) for ix in irange]
+	jobs = [(ix, job_server.submit(generate_snapshot,args=(ifname[ix],snapname[ix],proj,qvir,fdim,kval,xy_box,z_box,fcom_elements[ix]),depfuncs=(), modules=("matplotlib","numpy","mpl_toolkits"), globals=globals())) for ix in irange]
+        #(the "mpl_toolkits" is removed in another version of this code...
+        # not sure why, haven't needed to check. If problem, comment out)
 	for ix, job in jobs:
            px=job()
            del(px)
@@ -275,7 +293,7 @@ if ncores!=-1:
 parentdir = sim.split("/run")[0] #e.g. '../outputs/runinv_k01', gives 'outputs'
 os.system('mkdir -p '+parentdir+'/movies')
 
-os.system('avconv -y -r 24 -i ' + sim + '/snapshots/snap%04d.' + projection + '_' + str(xy_box) + 'pc.png -s 1024x800 ' + parentdir + '/movies/' + projection + '_' + str(kval) + '_' + str(xy_box) + 'pc.mp4')
+os.system('avconv -y -r 24 -i ' + sim + '/snapshots/snap%04d.' + proj + '_' + str(xy_box) + 'pc.png -s 1024x800 ' + parentdir + '/movies/' + proj + '_' + str(kval) + '_' + str(xy_box) + 'pc.mp4')
 
 
 #Remove png files:
