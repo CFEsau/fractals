6ea6c48eeaf3e7dbfb2999df9b3541e4ad4d9d01
Claire Esau <cfe@prometheus.shef.ac.uk>
2018-02-27 15:41:07 +0000

    Added some comments and paired=FALSE to wilcox.test for clarity

--- graphics/plots/stats/pvals.r
+++ graphics/plots/stats/pvals.r
@@ -24,24 +24,23 @@ for (k in 1:10) {
   simpath <- file.path(outpath,paste0('runinv_k',knum),cluster)
   
   plotsdir <- paste0(outpath,'/plots/cdf_',knum) #output directory for plots
-  #create output directory if it doesn't exist:
-  ifelse(!dir.exists(plotsdir), dir.create(plotsdir), FALSE)
+  ifelse(!dir.exists(plotsdir), dir.create(plotsdir), FALSE) #create output directory if doesn't exist
   
   # Input filenames for lambda data:
   fn3d <- 'allMSTs_lambar_3D.dat'; fnxy <- 'allMSTs_lambar_xy.dat'
   # Read in data as data frames:
   df3D <- read.table(file.path(simpath,'CDFdata/allMSTs_lambar_3D.dat'), row.names=1)
   dfxy <- read.table(file.path(simpath,'CDFdata/allMSTs_lambar_xy.dat'), row.names=1)
-  if (ncol(df3D) != ncol(dfxy)) stop("Data frames not of equal size")
-  nlambdas <- ncol(df3D)
-  nsnaps <- nrow(df3D)
+  if (ncol(df3D) != ncol(dfxy)) stop("Data frames not of equal size") #check data frame sizes
+  nlambdas <- ncol(df3D)  #data frames have 'nloop' columns
+  nsnaps <- nrow(df3D)    #and nsnaps rows
   
   # Build list of p-values for each test
   pvals <- data.frame() #(p_U=numeric(), p_t=numeric()) didn't work...
   for (i in 1:nsnaps) {
     pvals <- rbind(pvals, c(
-      #perform wilcoxon/U-test on data:
-      wilcox.test(as.numeric(df3D[i,]), as.numeric(dfxy[i,]))$p.value,
+      #perform mann-whitney-wilcoxon/U-test on data:
+      wilcox.test(as.numeric(df3D[i,]), as.numeric(dfxy[i,]),paired=FALSE)$p.value,
       #perform t-test on data:
       t.test(as.numeric(df3D[i,]), as.numeric(dfxy[i,]))$p.value
       )
@@ -53,13 +52,13 @@ for (k in 1:10) {
   pvals$U <- ifelse(pvals$U<1.e-99,1.e-99,pvals$U)
   pvals$t <- ifelse(pvals$t<1.e-99,1.e-99,pvals$t)
   
-  #Find p-vales that fall below 0.001 (3 sigma)
-  #If true, projection is in agreement with 3D distribution
-  pvals$agree_U <- ifelse(pvals$U<1.e-3,TRUE,FALSE)
-  pvals$agree_t <- ifelse(pvals$t<1.e-3,TRUE,FALSE)
+  #Find p-vales that fall above 0.001 (3 sigma)
+  #If true, projection is in agreement with 3D distribution (2D & 3D are same)
+  pvals$agree_U <- ifelse(pvals$U>1.e-3,TRUE,FALSE)
+  pvals$agree_t <- ifelse(pvals$t>1.e-3,TRUE,FALSE)
   
   #==========================
-  # Wilcoxon/U-test p-val plots:
+  # Wilcoxon/U-test p-val histograms:
   
   #countScale.ggplot <- ggplot(p_U$V1, aes(x = p-vals)) +  
   #  geom_histogram() +  
@@ -86,7 +85,7 @@ for (k in 1:10) {
     theme_bw()
   
   #==========================
-  # t-test p-val plots:
+  # t-test p-val histograms:
   
   #countScale.ggplot <- ggplot(p_t$V1, aes(x = p-vals)) +  
   #  geom_histogram() +  
@@ -121,17 +120,22 @@ for (k in 1:10) {
   plot(figure)
   #annotate_figure(figure, top=text_grob(paste0("k",knum), face="bold", size=10))
   dev.off()
+  
   #------------------------------------------------------------
+  #
   # Find fraction of lambdas that agree between projections
+  #
   nU <- 0; nt <- 0
+  #add 1 to nU / nt if agreement is TRUE
   nU <- ifelse(pvals$agree_U,nU+1,nU); nt <- ifelse(pvals$agree_t,nt+1,nt)
+  #divide number of TRUEs by number of random MSTs to find % agreement
   pfrac_U <- sum(nU)/nlambdas; pfrac_t <- sum(nt)/nsnaps
   
   #update knum'th row of data frame:
   p_knum[as.numeric(knum),] <- c(as.numeric(fdim)/10,as.numeric(qvir)/10,
                                  as.numeric(knum),pfrac_U,pfrac_t)
   
-}
+} #end of k loop
 
 #Format columns for output file:
 table_format <- c(rep("%.1f",2),"%2d",rep("%.3f",2))
@@ -149,7 +153,7 @@ abline(h=seq(from=0.5,to=1,by=0.05),col="lightgray",lty=2) #more control than 'g
 #dev.off() #close plot
 
 #'annotate_figure' isn't working in the loop - figure not updating.
-#May just be my version of Rstudio (1.1.383).
+#May just be my version of Rstudio (1.1.383). Could use ggplot instead...
 #In the meantime, load and annotate the png with knum, then overwrite.
 for (k in 1:10) {
   knum <- ifelse(k<10,paste0('0',as.character(k)),as.character(k))
