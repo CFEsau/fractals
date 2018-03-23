7995da7de812650be5986fd54fc0fbda0395e8f8
Claire Esau <cfe@prometheus.shef.ac.uk>
2018-03-23 15:34:37 +0000

    Combining boxplots.r and pvals.r to one piece of code (agreement.t) using functions to tidy it up, since boxplots needs pvals to be run first. Not yet working - just got plots working (in plots_fn.r)

---
+++ graphics/plots/stats/agreement.r
@@ -0,0 +1,241 @@
+#Calculate p-values
+library(data.table) # for melting data frames
+library(ggplot2)
+library(Hmisc) # for minor tick marks
+library(ggpubr) # for multiplot
+library(png) # for reading in & overwriting png at the end
+source('/local/cfe/backed_up_on_astro3/Github/fractals/graphics/plots/stats/plots_fn.r')
+
+
+find_agreement <- function(dfcol_method,dfcol_agree,method_type){
+  
+  if(method_type=='all'){
+    # Count number of snapshots tested, ntot:
+    ntot <- length(dfcol_agree)
+    # Count number of dfcol_agree where dfcol_agree is TRUE:
+    ntot_agree <- length(dfcol_agree[dfcol_agree=='TRUE'])
+    #Output fraction of time that projections are in agreement:
+    ntot_agree/ntot
+    
+  } else {
+    #using a specific method (pval, % tolerance, etc):
+    
+    #Count the number of times the method is used:
+    n_method <- length(dfcol_method[dfcol_method==method_type])
+    
+    #Count the number of times the method was *successful*:
+    nmethod_agree <- length(dfcol_method[dfcol_method==method_type &
+                                           dfcol_agree=='TRUE'])
+    
+    #Output fraction of time that lambda for 3D & 2D projection are in agreement:
+    nmethod_agree/n_method
+    
+  }
+}
+
+
+
+
+origin <- getwd()
+
+#Data frame for results: fdim | qvir | k | U | t | in10 | in20 | total
+# U & t: % of time p > 0.01 (using all snapshots regardless of lambda value)
+# in10: % of time 2D OR 3D are within 10% of each other when one lambda > 2
+# in20: % of time 2D AND 3D are within 20% of each other when both lambda > 2
+#'total': agreement % for U-test with lambda < 2 and gt10/20 for lambda >= 2
+tot_agreement <- data.frame('fdim'=numeric(),'qvir'=numeric(),'k'=integer(),
+                     'U'=numeric(), 'in10'=numeric(), 'in20'=numeric(), 'total'=numeric())
+
+# Build the directory structure:
+fbin <- 'fbinary0p0'
+fdim <- 'f16'; qvir <- 'q03'
+cluster <- 'cluster_FoV5pc'
+
+# General outputs directory: upper level for all simulation data, data analysis, and plots:
+outpath <- paste0('/local/cfe/backed_up_on_astro3/fractals/r1p0/',fbin,
+                  '/',fdim,qvir,'/analysis')
+
+#output directory for stats-related plots
+statsdir <- file.path(outpath,"stats")
+ifelse(!dir.exists(statsdir), dir.create(statsdir), FALSE)
+
+
+for (k in 1:10) {
+
+  # Directory containing analysed data:
+  knum <- ifelse(k<10,paste0('0',as.character(k)),as.character(k))
+  simpath <- file.path(outpath,paste0('runinv_k',knum),cluster)
+  
+  # Input filenames for lambda data:
+  fn3d <- 'allMSTs_lambar_3D.dat'; fnxy <- 'allMSTs_lambar_xy.dat'
+  
+  # Read in data as data frames:
+  df3D <- read.table(file.path(simpath,'CDFdata',fn3d), row.names=1)
+  dfxy <- read.table(file.path(simpath,'CDFdata',fnxy), row.names=1)
+  if (ncol(df3D) != ncol(dfxy)) stop("Data frames not of equal size") #check data frame sizes
+  nlambdas <- ncol(df3D)  #data frames have 'nloop' columns
+  nsnaps <- nrow(df3D)    #and nsnaps rows
+  
+  # Build list of p-values for each test, and list of median lambdas for 2D & 3D
+  pvals <- data.frame();  medianlam <- data.frame()
+  
+  for (i in 1:nsnaps) {
+    #===========================================
+    # Perform U-test & t-test for each snapshot
+    #===========================================
+    #make list of p-values for all snapshots
+    pvals <- rbind(pvals,
+                   c(
+                     #perform mann-whitney-wilcoxon/U-test on data:
+                     wilcox.test(as.numeric(df3D[i,]), as.numeric(dfxy[i,]),paired=FALSE)$p.value,
+                     #perform t-test on data:
+                     t.test(as.numeric(df3D[i,]), as.numeric(dfxy[i,]))$p.value
+                     )
+                   ) #end of pvals rbind
+    #Find median lambda for 2D & 3D.
+    medianlam <- rbind(medianlam,
+                       c(
+                         median(as.numeric(df3D[i,])), median(as.numeric(dfxy[i,]))
+                         )
+                       )
+    } #end of nsnaps loop
+  
+  colnames(pvals) <- c( 'U', 't')
+  colnames(medianlam) <- c('med_3D', 'med_2D')
+  
+  #Change p-values of 0 to be really really small
+  pvals$U <- ifelse(pvals$U<1.e-99,1.e-99,pvals$U)
+  pvals$t <- ifelse(pvals$t<1.e-99,1.e-99,pvals$t)
+  
+  #Find p-vales above 0.001 (3 sigma)
+  #If true, projection is in agreement with 3D distribution
+  pvals$agree_U <- ifelse(pvals$U>1.e-3,TRUE,FALSE)
+  pvals$agree_t <- ifelse(pvals$t>1.e-3,TRUE,FALSE)
+  
+  #---------------
+  #populate 'medianlam' df with TRUE/FALSE for agreement within 10%/20%:
+  
+  #make 1D data structures with max median lambda & min median lambda for comparing with 2.0 boundary
+  largest <- pmax(medianlam$med_3D,medianlam$med_2D)
+  smallest <- pmin(medianlam$med_3D,medianlam$med_2D)
+  
+  #note on rounding: the 'equals' bit probably won't work due to floating point problems.
+  #Instead could use 'round' - e.g. 3*0.8==2.4 gives FALSE, but round(3*0.8,2)==round(2.4,2) gives TRUE.
+  #Similarly, could do something with tol <- 1e-2; abs(x-y) <= tol
+  #Maybe implement later on, e.g. with #d.p.=3 or 4, or 'tol'=1e-4...
+  
+  #If the largest of the 2D and 3D values are >= 2.0, see if the smaller lies within 10%:
+  medianlam$one_gt2_10 <- ifelse(largest >= 2.0 & smallest < 2.0,
+    ifelse(smallest >= 0.9*largest & smallest <= 1.1*largest,TRUE,FALSE),'NA')
+  
+  #If both 2D and 3D are >= 2.0, see if smaller lies within 20% of larger:
+  medianlam$two_gt2_20 <- ifelse(smallest >= 2.0,
+    ifelse(smallest >= 0.8*largest & smallest <= 1.2*largest,TRUE,FALSE),'NA')
+  
+  
+  #Combine pvals & medianlam as master data frame with TRUE/FALSE for each snapshot
+  # depending on comparison method
+  snaps_test <- cbind(pvals,medianlam)
+  #if both 2D & 3D median lambda <2, use p-value
+  snaps_test$method <- ifelse(largest<2.0,'pval',
+                              #if medianlam$one_gt2 is TRUE/FALSE (i.e. not NA), 10% difference is used:
+                              ifelse(medianlam$one_gt2!='NA','10%',
+                                     #as above, for 20% difference between lambdas:
+                                     ifelse(medianlam$two_gt2!='NA','20%',
+                                            'unknown')) #if none of the above, 'unknown' (something is wrong!)
+                              )
+  snaps_test$in_agreement <- ifelse(snaps_test$method == 'pval',snaps_test$agree_U,
+                                 ifelse(snaps_test$method == '10%',snaps_test$one_gt2,
+                                        ifelse(snaps_test$method == '20%',snaps_test$two_gt2,
+                                               'NA')) #should all be T/F. An 'NA' here means something is wrong.
+                                 )
+  
+  #----------------------------------------------------------#
+  # Find fractions of time that projections are in agreement #
+  #----------------------------------------------------------#
+  
+  #Count the number of times each method is used using 'find_agreement' function.
+  #Pass in data frame columns containing method types and T/F, and one method type
+  
+  frac_p <- find_agreement(snaps_test$method,snaps_test$in_agreement,'pval')
+  frac_10pc <- find_agreement(snaps_test$method,snaps_test$in_agreement,'10%')
+  frac_20pc <- find_agreement(snaps_test$method,snaps_test$in_agreement,'20%')
+  #Total number of snapshots in agreement for this k:
+  frac_agree <- find_agreement(snaps_test$method,snaps_test$in_agreement,'all')
+  
+  
+  #-----------------------
+  #Results:
+  
+  #update knum'th row of data frame with % agreement for each method
+  tot_agreement[as.integer(knum),] <- c(as.numeric(fdim)/10,as.numeric(qvir)/10,
+                                    as.integer(knum),frac_p,frac_10pc,frac_20pc,frac_agree)
+  
+  
+  
+  ###############################################################
+  #                                                             #
+  #                        PLOTS                                #
+  #                                                             #
+  ###############################################################
+  
+  #===================================
+  # Lambda(t) showing 3D & 2D lines with colour coding for T/F:
+  
+  timeseries_fn(df=snaps_test) #uses median lambdas, saved in both medianlam and snaps_test.
+  
+  #===================================
+  # Histograms of p-values from U-test(pval vs. count)
+  histUt_fn(df=pvals)
+  
+  # Histograms of p-values from U- and t-tests(pval vs. count)
+  histUt_fn(df=pvals,ttest=TRUE)
+  
+} #end of k loop
+
+#Format columns for output file.
+tot_agreement[,3] <- sapply(tot_agreement[,3],as.integer) #Change column 3 (knum) to 'integer'
+table_format <- c(rep("%.1f",2),"%2d",rep("%.3f",4))      #Set formatting
+agreement_fmt <- tot_agreement                            #Copy df so unformatted one not overwritten
+agreement_fmt[] <- mapply(sprintf, table_format, tot_agreement) #Make new formatted df
+
+#Output dataframe as table with p-vals to 4 d.p.
+write.table(agreement_fmt,file=file.path(statsdir,"pvals.dat"),
+            quote=FALSE, row.names=FALSE, sep="\t")
+
+
+#box plot of all results across k for this parameter set.
+#x-axis: U-test box plot, t-test box plot, within 10% & >2 box plot, 'total' in agreement.
+png(filename=paste0(statsdir,"/boxplot_f",fdim,"q",qvir,".png"))
+
+boxplot(tot_agreement$U,tot_agreement$in10,tot_agreement$in20,tot_agreement$total,
+        names=c("U",expression(paste(Lambda,"_10%")),
+                expression(paste(Lambda,"_20%")),
+                "total"),range=0,ylim=c(0,1.0))#0.6))
+
+minor.tick(ny=4,tick.ratio=0.3)
+#Grid lines:
+#grid(NA,NULL,col="lightgray",lty=2) #nx=NA; ny=NULL (defult major tick positions)
+abline(h=seq(from=0.0,to=1.0,by=0.05),col="lightgray",lty=2) #more control than 'grid'
+
+dev.off() #close plot
+
+#'annotate_figure' isn't working in the loop - figure not updating.
+#May just be my version of Rstudio (1.1.383). Could use ggplot instead...
+#In the meantime, load and annotate the png with knum, then overwrite.
+for (k in 1:10) {
+  knum <- ifelse(k<10,paste0('0',as.character(k)),as.character(k))
+  
+  img <- readPNG(paste0(statsdir,"/pvals_",knum,".png"))
+  h <- dim(img)[1]
+  w <- dim(img)[2]
+  png(paste0(statsdir,"/pvals_",knum,".png"),width=w,height=h)
+  par(mar=c(0,0,0,0),xpd=NA,mgp=c(0,0,0),oma=c(0,0,0,0),ann=F)
+  plot.new()
+  plot.window(0:1,0:1)
+  usr <- par("usr")
+  rasterImage(img, usr[1], usr[3], usr[2], usr[4]*0.95)
+  text(.52,1.01,paste0("k",knum),cex=1)
+  
+  dev.off()
+}
---
+++ graphics/plots/stats/plots_fn.r
@@ -0,0 +1,81 @@
+#Used in pvals.r
+
+timeseries_fn <- function(df,xmin=0,xmax=10,dx=1,ymin=0,ymax=15,dy=2,
+                          bckcols=c("TRUE" = "green", "FALSE" = "red")){
+  
+  # Add 'time_Myr' column to data frame: 0.01 Myr per row
+  df <- cbind(time_Myr=as.numeric(row.names(df))*0.01,df)
+
+  df_melt <- melt(df, id=c("time_Myr","method","in_agreement"),
+                     measure=c("med_3D","med_2D"),
+                     variable="dimension", value.name="medianlam") #rename new columns
+  
+  
+  timeseries <- ggplot() +
+    geom_line(data=df_melt,aes(x=time_Myr,y=medianlam,group=dimension),
+            colour="gray20") +
+    #for colour: geom_line(aes(color=dimension)) +  #colour not relevant - doesn't matter which is 3D/2D
+    #scale_color_manual(values=c("brown1", "blue2"))
+    geom_rect(data=df,aes(xmin=time_Myr-0.01,xmax=time_Myr,
+                                  ymin=-Inf,ymax=ymax,fill=in_agreement),alpha=0.2) +
+    scale_color_manual(values=bckcols) +
+    theme_minimal() +
+    theme(legend.position="none",
+          panel.grid.minor = element_blank()) + #remove minor grid lines
+    scale_x_continuous(breaks=seq(xmin,xmax,dx)) +
+    scale_y_continuous(breaks=seq(ymin,ymax,dy))
+  
+  png(filename=paste0(statsdir,"/lambda_",fdim,"q",qvir,"_k",knum,".png"))
+  plot(timeseries)
+  dev.off()
+}
+
+
+
+histUt_fn <-function(df,xmax1=1,xmax2=1,xmin1=1e-40,xmin2=1e-12,
+                     xlab="log(p)",ylab="nsnaps",ttest=FALSE){
+  
+  # Wilcoxon/U-test p-val histograms:
+  plot1U <- histplot_fn(df,plotdat="U",xmin=xmin1,xmax=xmax1,xlab,ylab)
+  plot2U <- histplot_fn(df,plotdat="U",xmin=xmin2,xmax=xmax2,xlab,ylab)
+  
+  
+  if(ttest==TRUE){
+    
+  # t-test p-val histograms:
+  plot1t <- histplot_fn(df,plotdat="t",xmin=xmin1,xmax=xmax1,xlab,ylab)
+  plot2t <- histplot_fn(df,plotdat="t",xmin=xmin2,xmax=xmax2,xlab,ylab)
+  
+  # Add plots (ggarrange adds multiple to same page)
+  figure <- ggarrange(plot1U, plot1t, plot2U, plot2t,
+                      labels = c("U", "t", "U", "t"),
+                      ncol = 2, nrow = 2)
+  
+  } else {
+    
+    figure <- ggarrange(plot1U, plot2U, labels = c("U", ""),ncol = 2, nrow = 1)
+  }
+  #png(filename=paste0(statsdir,"/pvals_",knum,".png"))
+  plot(figure)
+  #annotate_figure(figure, top=text_grob(paste0("k",knum), face="bold", size=10))
+  
+  #dev.off()
+}
+
+histplot_fn <-function(df,plotdat,xmin,xmax,xlab,ylab){
+  
+  #countScale.ggplot <- ggplot(p_t$V1, aes(x = p-vals)) +  
+  #  geom_histogram() +  
+  #  ggtitle("(2) countScale with ggplot") +  
+  #  scale_x_log10()
+  
+  ggplot(data=df,aes_string(x=plotdat)) + geom_histogram(binwidth=1.e0) +
+    scale_x_log10(limits = c(xmin, xmax),
+                  breaks = scales::trans_breaks("log10", function(x) 10^x),
+                  labels = scales::trans_format("log10", scales::math_format(.x))) + #or format (10^.x)
+    #expand=c(0,0)) + #(change close par to comma on above line if uncommenting)
+    labs(x = xlab) +
+    scale_y_continuous(expand=c(0,0)) +
+    theme_bw() #+
+  #annotation_logticks(sides='b')   #log ticks on x-axis
+}
