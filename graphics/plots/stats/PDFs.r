146f55e75bc210db073cf1a91f2069f8e772144c
Claire Esau <cfe@prometheus.shef.ac.uk>
2018-05-10 17:49:20 +0100

    more minor style changes

--- graphics/plots/stats/PDFs.r
+++ graphics/plots/stats/PDFs.r
@@ -7,7 +7,7 @@ fn3d <- 'allMSTs_lambar_3D'
 proj <- 'xy' #currently doesn't do anything. Change to variable later.
 fn2d <- 'allMSTs_lambar_xy'
 cluster <- 'cluster_FoV5pc'
-outpath <- paste0('/local/cfe/backed_up_on_astro3/fractals/r1p0/fbinary0p0/f16q03/analysis')
+outpath <- '/local/cfe/backed_up_on_astro3/fractals/r1p0/fbinary0p0/f16q03/analysis'
 
 #select snapshots
 #knum <- 'k01'
@@ -17,7 +17,7 @@ outpath <- paste0('/local/cfe/backed_up_on_astro3/fractals/r1p0/fbinary0p0/f16q0
 #knum <- 'k03'
 #snapshots <- c(58, 77, 400, 425)
 knum <- 'k10'
-snapshots <- c(25:42,60:80,120:135)
+snapshots <- c(25:42, 60:80, 120:135)
 
 simpath <- file.path(outpath, paste0('runinv_', knum), cluster)
 plotsdir <- paste0(outpath, '/plots/cdf_', knum) #outputs directory for plots
@@ -41,27 +41,28 @@ for (snap in 1:length(snapshots)){
   colnames(df)[1] <- '3D'; colnames(df)[2] <- '2D'
   df_long <- melt(df) #convert data to long format
   
-  pval <- wilcox.test(as.numeric(lambdas3d[snap, ]),as.numeric(lambdas2d[snap, ]), paired = FALSE)$p.value
+  pval <- wilcox.test(as.numeric(lambdas3d[snap, ]), as.numeric(lambdas2d[snap, ]), paired = FALSE)$p.value
   
   #set up output plot:
   outfn_pdf <- sprintf('pdf%02s_snap%04d.png', proj, snapshots[snap]) # output file name
-  png(filename = file.path(plotsdir, outfn_pdf), width = 500, height = 500)#,res=40)
+  png(filename = file.path(plotsdir, outfn_pdf), width = 500, height = 500)#, res=40)
   
   plot(
-    ggplot(df_long, aes(x = value,color = variable)) +
+    ggplot(df_long, aes(x = value, color = variable)) +
       geom_line(stat = "density") +
       scale_color_manual(values = c("black", "red")) +
-      geom_vline(xintercept = median(lambdas3d[snap, ]),colour = "black") + #line at median 3D point
-      geom_vline(xintercept = median(lambdas2d[snap, ]), colour = "red", linetype = "dotted") + #line at median 3D point
+      #lines at median 3D & 2D positions:
+      geom_vline(xintercept = median(lambdas3d[snap, ]), colour = "black") +
+      geom_vline(xintercept = median(lambdas2d[snap, ]), colour = "red", linetype = "dotted") +
       annotate("rect", xmin = median(lambdas3d[snap, ]) * 0.9, xmax = median(lambdas3d[snap, ]) * 1.1,
            ymin = -Inf, ymax = Inf, alpha = .2, fill = "blue") + #shaded region 10% around 3D med
       theme_minimal()  +
       scale_x_continuous(breaks = scales::pretty_breaks(n = 6)) +
       #annotate with p-value & snapshot number:
-      annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1, label=sprintf("p = %4.2e", pval)) +
-      annotate("text", x = Inf, y = Inf, hjust = 1,vjust = 3.2, size = 3.6,
+      annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1, label = sprintf("p = %4.2e", pval)) +
+      annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 3.2, size = 3.6,
              label = ifelse(pval > 1.e-3, TRUE, FALSE)) +
-      annotate("text", x= -Inf, y = Inf, hjust = 0, vjust = 1, label = sprintf("snap %04d", snapshots[snap]))
+      annotate("text", x = -Inf, y = Inf, hjust = 0, vjust = 1, label = sprintf("snap %04d", snapshots[snap]))
   )
   dev.off() #close plot
 }
