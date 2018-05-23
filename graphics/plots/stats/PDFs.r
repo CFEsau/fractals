785729099284e1d15942e9373ff5116bbd389f7f
Claire Esau <cfe@prometheus.shef.ac.uk>
2018-05-23 14:55:27 +0100

    plot save location changed, added python script to produce plots of time series showing snapshot and errors corresponding to given PDF

--- graphics/plots/stats/PDFs.r
+++ graphics/plots/stats/PDFs.r
@@ -1,45 +1,60 @@
 library(ggplot2)
 library(reshape2)
+library(matrixStats) # for rowMedians
 
 origin <- getwd()
+
+fstr <- 'f16'
+qstr <- 'q03'
 
 fn3d <- 'allMSTs_lambar_3D'
 proj <- 'xy' #currently doesn't do anything. Change to variable later.
 fn2d <- 'allMSTs_lambar_xy'
-cluster <- 'cluster_FoV5pc'
-outpath <- '/local/cfe/backed_up_on_astro3/fractals/r1p0/fbinary0p0/f16q03/analysis'
+cluster <- 'cluster_FoV5pc' # use 5 pc field of view
+
+#'root' directory:
+rootdir <- '/local/cfe/backed_up_on_astro3/fractals/r1p0/fbinary0p0'
 
 #select snapshots
 #knum <- 'k01'
-#snapshots <- c(76, 105, 135, 459)
-#knum <- 'k02'
-#snapshots <- c(200, 275, 400, 420)
+#snapshots <- c(1:220, 315:349)
+knum <- 'k02'
+snapshots <- c(490:520)
 #knum <- 'k03'
 #snapshots <- c(58, 77, 400, 425)
-knum <- 'k10'
-snapshots <- c(25:42)#, 60:80, 120:135)
+#knum <- 'k07'
+#snapshots <- c(520:649)
+#knum <- 'k10'
+#snapshots <- c(25:42, 60:80, 120:135)
 
-simpath <- file.path(outpath, paste0('runinv_', knum), cluster)
-plotsdir <- paste0(outpath, '/plots/pdf_', knum) #outputs directory for plots
+#input (data) and output (plots) directories:
+inpath <- file.path(rootdir, paste0(fstr, qstr, '/analysis/runinv_', knum), cluster)
+outpath <- file.path(rootdir, 'stats', paste0('pdf_', fstr, qstr, '_', knum))
 #create output directory if it doesn't exist:
-ifelse(!dir.exists(plotsdir), dir.create(plotsdir), FALSE)
+ifelse(!dir.exists(outpath), dir.create(outpath), FALSE)
 
 nmsts <- 1000 #don't *need* this, it just helps with memory allocation. Might as well, since it's known
 
-nsnaps <- length(readLines(file.path(simpath, 'CDFdata/allMSTs_lambar_3D.dat')))
+nsnaps <- length(readLines(file.path(inpath, 'CDFdata/allMSTs_lambar_3D.dat')))
 
-all_lambdas3d <- read.table(file.path(simpath, 'CDFdata/allMSTs_lambar_3D.dat'),
+all_lambdas3d <- read.table(file.path(inpath, 'CDFdata/allMSTs_lambar_3D.dat'),
                             nrows = nsnaps, row.names = 1, header = FALSE,
-                            col.names = c("snap",sprintf("snap%04d", 1:nmsts)))
+                            col.names = c("snap", sprintf("snap%04d", 1:nmsts)))
 
-all_lambdas2d <- read.table(file.path(simpath, 'CDFdata/allMSTs_lambar_xy.dat'),
+all_lambdas2d <- read.table(file.path(inpath, 'CDFdata/allMSTs_lambar_xy.dat'),
                             nrows = nsnaps, row.names = 1, header = FALSE,
-                            col.names = c("snap",sprintf("snap%04d", 1:nmsts)))
+                            col.names = c("snap", sprintf("snap%04d", 1:nmsts)))
 
 #get data for each snapshot: nloop lambdas for each snapshot
 # Order: [row, column] - data saved for rows specified in 'snapshots', all columns.
 my_lambdas3d <- data.matrix(all_lambdas3d[snapshots, ])
 my_lambdas2d <- data.matrix(all_lambdas2d[snapshots, ])
+
+medians_3d <- data.frame(rowMedians(as.matrix(all_lambdas3d)))
+medians_3d <- gsub("c|\\(|\\)|\n|,", "", medians_3d) #remove c, parentheses, \n, and commas from string
+medians_2d <- data.frame(rowMedians(as.matrix(all_lambdas2d)))
+medians_2d <- gsub("c|\\(|\\)|\n|,", "", medians_2d) #remove c, parentheses, \n, and commas from string
+
 
 #kernel density estimates
 #(see https://www.r-bloggers.com/the-density-function/ for good summary)
@@ -52,8 +67,8 @@ for (snap in 1:length(snapshots)){
   pval <- wilcox.test(as.numeric(my_lambdas3d[snap, ]), as.numeric(my_lambdas2d[snap, ]), paired = FALSE)$p.value
   
   #set up output plot:
-  outfn_pdf <- sprintf('pdf%02s_snap%04d.png', proj, snapshots[snap]) # output file name
-  png(filename = file.path(plotsdir, outfn_pdf), width = 500, height = 500)#, res=40)
+  outfn_pdf <- sprintf('snap%04dpdf_%02s.png', snapshots[snap], proj) # output file name
+  png(filename = file.path(outpath, outfn_pdf), width = 500, height = 500)#, res=40)
   
   plot(
     ggplot(df_long, aes(x = Lambda, color = Dimension)) +
@@ -73,8 +88,62 @@ for (snap in 1:length(snapshots)){
       annotate("text", x = -Inf, y = Inf, hjust = 0, vjust = 1, label = sprintf("snap %04d", snapshots[snap]))
   )
   dev.off() #close plot
+  
+  #Plot lambda time-series showing this snapshot
+  quartiles3d <- paste(quantile(df_lambdas[, 1]), collapse = ' ') #concaternate 'quantile' vector to one string
+  quartiles2d <- paste(quantile(df_lambdas[, 2]), collapse = ' ')
+  sixths3d <- paste(quantile(df_lambdas[, 1], probs = seq(0, 1, 1/6)), collapse = ' ') #concaternate 'quantile' vector to one string
+  sixths2d <- paste(quantile(df_lambdas[, 2], probs = seq(0, 1, 1/6)), collapse = ' ')
+  system(paste('python PDFs_lambda.py', outpath, knum, cluster, snapshots[snap], #model variables
+               '--medians3d', medians_3d,
+               '--medians2d', medians_2d,
+               '--quartiles3d', quartiles3d,
+               '--quartiles2d', quartiles2d,
+               '--sixths3d', sixths3d,
+               '--sixths2d', sixths2d
+         ), wait=FALSE)
+  
+  #get y-values at given x-value at various points to find any snaps that don't overlap
+  #(to check whether 2D & 3D 'agree' for paper purposes)
+  # Get coordinates of density function for 3D & 2D lambdas
+  dens3D <- density(df_lambdas[, 1])
+  dens2D <- density(df_lambdas[, 2])
+  dens3D_y <- approxfun(dens3D$x, dens3D$y)
+  dens2D_y <- approxfun(dens2D$x, dens2D$y)
+  
+  #demonstrate method to find point on density function using median as example:
+  hist(df_lambdas[, 1], freq = F)
+  lines(dens3D, col="red", lwd=2)
+  abline(v=median(df_lambdas[, 1]), lty=2)
+  points(median(df_lambdas[, 1]), dens3D_y(median(df_lambdas[, 1])),
+         cex=1.2, pch=20, col="blue")
+  points(min(df_lambdas[, 1]), dens3D_y(min(df_lambdas[, 1])),
+         cex=1.2, pch=20, col="blue")
+  points(max(df_lambdas[, 1]), dens3D_y(max(df_lambdas[, 1])),
+         cex=1.2, pch=20, col="blue")
+  #Use 1/6 & 5/6 (0.17, 0.83) rather than quartiles:
+  points(quantile(df_lambdas[, 1], 0.17), dens3D_y(quantile(df_lambdas[, 1], 0.17)),
+         cex=1.2, pch=20, col="blue")
+  points(quantile(df_lambdas[, 1], 0.83), dens3D_y(quantile(df_lambdas[, 1], 0.83)),
+         cex=1.2, pch=20, col="blue")
+  #draw 2D density function:
+  lines(dens2D, col="black", lwd=2)
+  #dev.off()
+  
+  #compare y-values of 2D & 3D density plots at max, min, median, & quartiles
+  denspoints_3D <- c(dens3D_y(min(df_lambdas[, 1])),
+                     dens3D_y(quantile(df_lambdas[, 1], .25)),
+                     dens3D_y(median(df_lambdas[, 1])),
+                     dens3D_y(quantile(df_lambdas[, 1], .75)),
+                     dens3D_y(max(df_lambdas[, 1]))
+  )#keep df_lambdas[, 1] for 2D check as x-values need to be the same
+  denspoints_2D <- c(dens2D_y(min(df_lambdas[, 1])),
+                     dens2D_y(quantile(df_lambdas[, 1], .25)),
+                     dens2D_y(median(df_lambdas[, 1])),
+                     dens2D_y(quantile(df_lambdas[, 1], .75)),
+                     dens2D_y(max(df_lambdas[, 1]))
+  )
 }
-
 
 #'ecdf': Empirical Cumulative Distribution Function
 #for (snap in 1:length(snapshots)) {
@@ -82,7 +151,7 @@ for (snap in 1:length(snapshots)){
 #  CDF2 <- ecdf(my_lambdas2d[snap, ])
 #  outfn_cdf <- sprintf('lambdas%02s_snap%04d.png', proj, snapshots[snap]) # output file name
 #  #set up output plot:
-#  png(filename = file.path(plotsdir, outfn_cdf), width = 500, height = 500)#, res=40)
+#  png(filename = file.path(outpath, outfn_cdf), width = 500, height = 500)#, res=40)
 #  plot(CDF1, do.points = FALSE) #plot CDF1
 #  lines(CDF2, col='red', do.points = FALSE) #overplot CDF2
 #  legend("topleft", legend = c("3D", "xy"), col = c('black', 'red'), lty = c(1, 1))
---
+++ graphics/plots/stats/PDFs_lambda.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+from __future__ import division #import division from V3 to avoid divide by 0
+import os, sys
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib import container #needed to remove error bars from legend
+from matplotlib.ticker import AutoMinorLocator #for minor ticks
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Lambda for median MST')
+
+parser.add_argument('model_info',nargs='+')
+
+parser.add_argument('--medians3d', nargs='*', type=float, default=[])
+parser.add_argument('--medians2d', nargs='*', type=float, default=[])
+
+parser.add_argument('--quartiles3d', nargs=5, type=float)
+parser.add_argument('--quartiles2d', nargs=5, type=float)
+
+parser.add_argument('--sixths3d', nargs=7, type=float)
+parser.add_argument('--sixths2d', nargs=7, type=float)
+
+args = parser.parse_args()
+
+plotsdir = args.model_info[0]   # or sys.argv[1]
+kval = args.model_info[1]      # or sys.argv[2]
+ctype = args.model_info[2].split("_")[1] #remove 'cluster_' prefix
+
+medians3d = args.medians3d
+medians2d = args.medians2d
+nsnaps = len(args.medians3d)
+
+thissnap = int(args.model_info[3])
+#print args.quartiles3d
+#print args.quartiles2d
+
+#print plotsdir
+#print kval
+#print ctype
+
+mpl.rcParams['lines.linewidth'] = 1.0 #set default line width to 1.0
+#
+projections=['3D','xy']
+duration = 10 #duration in Myr (x-axis limit)
+
+#Make plot:
+
+fig, ax = plt.subplots()
+#plt.figure()
+plt.xlabel("Time (Myr)")
+plt.ylabel('$\overline{\Lambda}$')
+plt.title("Mass segregation")
+#n-1 minor ticks:
+ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+#plt.xlim(0,duration)
+#plt.ylim(0,8)
+xmin = 2 ; xmax = 6
+plt.xlim(xmin,xmax)
+plt.ylim(0,4)
+
+tmin = duration/nsnaps
+#add extra element to max value as 'arange' goes to max+1:
+time = np.arange(tmin, duration + tmin, duration/nsnaps)
+
+#print thissnap
+#print(time[thissnap],medians3d[thissnap],
+#      args.quartiles3d[1],args.quartiles3d[3])
+
+
+ax.plot(time,medians3d,label='3D')
+ax.plot(time,medians2d,label='xy')
+
+#reset colour cycle:
+plt.gca().set_prop_cycle(None)
+
+plt.plot(time[thissnap-1],medians3d[thissnap],marker='.')
+
+plt.plot(time[thissnap-1],medians2d[thissnap],marker='.')
+
+#reset colour cycle:
+plt.gca().set_prop_cycle(None)
+
+#error bars at quartiles:
+err3d_lo = medians3d[thissnap] - args.quartiles3d[1]
+err3d_hi = args.quartiles3d[3] - medians3d[thissnap]
+err2d_lo = medians2d[thissnap] - args.quartiles2d[1]
+err2d_hi = args.quartiles2d[3] - medians2d[thissnap]
+#error bars at 1/6, 5/6:
+#err3d_lo = medians3d[thissnap] - args.sixths3d[1]
+#err3d_hi = args.sixths3d[-2] - medians3d[thissnap]
+#err2d_lo = medians2d[thissnap] - args.sixths2d[1]
+#err2d_hi = args.sixths2d[-2] - medians2d[thissnap]
+
+#shift error bars by tiny amount either side to avoid overlap
+errshift = 0.002*(xmax-xmin)
+
+plt.errorbar(time[thissnap-1]+errshift,medians3d[thissnap],marker='None',
+             yerr=[[err3d_lo],[err3d_hi]],
+             capsize=4
+)[-1][0].set_linestyle('--')
+
+plt.errorbar(time[thissnap-1]-errshift,medians2d[thissnap],marker='None',
+             yerr=[[err2d_lo],[err2d_hi]],
+             capsize=4
+)[-1][0].set_linestyle('--')
+
+plt.legend(loc="upper right", fontsize=10)
+
+plt.tight_layout() #smaller margins
+#plt.show()
+saveplot = ("%s/snap%04d_lambda_quartiles.png" % (plotsdir,thissnap))
+plt.savefig(saveplot, bbox_inches='tight')
+plt.close()
